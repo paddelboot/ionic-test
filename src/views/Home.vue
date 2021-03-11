3ec6715ff400f2a704ea3fea3dac46b58a76e464
Michael Schröder <632353+paddelboot@users.noreply.github.com>
2021-03-11 14:57:46 +0100

    Inital commit

---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <ion-page>
+
+      <ion-header>
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-menu-button
+              auto-hide="false"
+              menu="testmenu"
+            ></ion-menu-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-menu menu-id="testmenu" content-id="mycontent">
+        <ion-content>
+          <ion-list>
+            <ion-item type="button">
+              <ion-label>Pokémon Yellow</ion-label>
+            </ion-item>
+          </ion-list>
+        </ion-content>
+      </ion-menu>
+
+      <ion-content id="mycontent"> Router Outlet </ion-content>
+
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonToolbar,
+  IonMenuButton,
+  IonButtons,
+  IonList,
+  IonItem,
+  IonMenu,
+  IonLabel,
+  IonContent,
+  IonHeader,
+  IonPage
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    IonToolbar,
+    IonMenuButton,
+    IonButtons,
+    IonList,
+    IonItem,
+    IonMenu,
+    IonLabel,
+    IonContent,
+    IonHeader,
+    IonPage
+  },
+});
+</script>
+
+<style scoped >
+</style>
